1098c3a54049cf9ad90e2761dacc165bd4a16e02
auronmatrix <auronmatrix@gmail.com>
2016-11-17 11:56:27 +0100

    Setup with IIS

---
+++ TehBoht/TehBoht/Dialogs/EchoDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TehBoht.Dialogs
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            await context.PostAsync("You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
